a59a0e8d2dc23f1000a772a082a3922651fb1c1d
ogbk <ogbu.k.olu@gmail.com>
2020-06-07 23:45:09 +0100

    edit App

--- src/App.js
+++ src/App.js
@@ -1,45 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import './sass/styles.sass';
 
 function App() {
+  const DEFAULT_PROFILE = 'ogbu_olu';
 
+  const [profileId, setProfileId] = useState(DEFAULT_PROFILE);
+  const [profileActive, setProfileActive] = useState(false);
   const [picture, setPicture] = useState(undefined);
-  const [profile, setProfile] = useState({});
+  const [profile, setProfile] = useState(undefined);
 
-  useEffect(
-    () => {
-      const fetchData = async () => {
-        const res = await fetch('/api/ogbu_olu');
+  const handleChange = ({ target: { value } }) => {
+    setProfileId(value);
+  };
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+
+    try {
+      const res = await fetch(`/api/${profileId}`);
+
+      if (res.ok && res.status === 200) {
         const json = await res.json();
         setProfile(json);
-        
-        const resPic = await fetch('/api/img/ogbu_olu');
+
+        const resPic = await fetch(`/api/img/${profileId}`);
         const picBlob = await resPic.blob();
         const pic = await URL.createObjectURL(picBlob);
         setPicture(pic);
-      };
-
-      fetchData();
-    },
-    []
-  );
+        setProfileActive(true);
+      } else {
+        setProfileActive(false);
+      }
+    } catch (err) {
+      setProfileActive(false);
+    }
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={picture}  alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form onSubmit={handleSubmit}>
+        <span>Find karate champion: </span>
+        <select value={profileId} onChange={handleChange}>
+          <option value="ogbu_olu"> Ogbu Olu </option>
+          <option value="chuck_norris"> Chuck Norris </option>
+        </select>
+        <button type="submit" className="click"> GET PROFILE </button>
+      </form>
+
+      <br />
+      { profileActive && <img src={picture} alt="Profile" /> }
     </div>
   );
 }
