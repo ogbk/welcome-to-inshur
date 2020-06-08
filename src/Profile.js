aae865d33ceaf131f4d777bc1e2ba914fe8e6ad5
ogbk <ogbu.k.olu@gmail.com>
2020-06-08 11:24:49 +0100

    display profile details

--- src/Profile.js
+++ src/Profile.js
@@ -1,3 +1,5 @@
+// @flow
+
 import React, { useState, useEffect } from 'react';
 
 function Profile({ profile, profileId }) {
@@ -11,22 +13,34 @@ function Profile({ profile, profileId }) {
         const pic = await URL.createObjectURL(picBlob);
         setPicture(pic);
       };
-
       fetchData();
     },
     [],
   );
 
+  const {
+    name, title, belts, 'favourite food': fav_food,
+  } = profile;
+
   return (
-    <div className="profile_page">
-      <div>
-        <img src={picture} alt="Profile" />
+    <div className="profile-page">
+      <img src={picture} alt="Profile" />
+      <div className="profile-details">
+        <div className="field-key">name</div>
+        <div className="field-value">{name}</div>
+        <div className="field-key">title</div>
+        <div className="field-value">{title}</div>
+        <div className="field-key">favourite food</div>
+        <div className="field-value">{fav_food}</div>
+        <div className="field-key">belts</div>
+        <ul>
+          {
+            belts.map((belt, idx) => (
+              <li key={`${idx}-${belt}`} className="field-value-list">{belt}</li>
+            ))
+          }
+        </ul>
       </div>
-      <div>
-        <h1>dadad</h1>
-      </div>
-
-
     </div>
   );
 }
